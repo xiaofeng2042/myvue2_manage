939fdea199d712506d803942cc5ed3019c338478
fxf <601244325@qq.com>
2018-02-26 09:39:19 +0800

    去掉webpack的热更新模块

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,4 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   NODE_HOST: '"http://47.75.8.92:32776"'
+  // NODE_HOST: '"http://127.0.0.1:3000"'
 })
